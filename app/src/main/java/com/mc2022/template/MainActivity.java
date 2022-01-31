e54e1ea0eca3aad89e345a1b9ceb373f563cf8bb
meet18091998 <meet18091998@gmail.com>
2022-01-31 19:07:17 +0530

    Added shared preferences.

--- app/src/main/java/com/mc2022/template/MainActivity.java
+++ app/src/main/java/com/mc2022/template/MainActivity.java
@@ -2,13 +2,42 @@ package com.mc2022.template;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    int openCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPref = getSharedPreferences("my-application", MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        openCount = sharedPref.getInt("open-count-new", 0);
+        openCount += 1;
+        TextView tv = (TextView) findViewById(R.id.openCount);
+
+        tv.setText("Opened " + openCount + " times!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences.Editor ed = sharedPref.edit();
+        ed.putInt("open-count-new", openCount);
+        ed.apply();
     }
 }
